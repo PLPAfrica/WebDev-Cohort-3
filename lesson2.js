7f4838c3a0a62590691db07f92af8192f113c8ba
Evans-mutuku <evansnyamai98@gmail.com>
2023-04-14 16:12:48 +0300

    lesson 2 done

---
+++ lesson2.js
@@ -0,0 +1,18 @@
+let x = 2;
+
+while (x <= 9) {
+  console.log("The value of X: " + x + "\n");
+  x++;
+}
+
+for (let y = 8; y <= 20; y++) {
+  console.log("The value of Y: ", y);
+}
+
+var i = 30;
+do {
+  // The line while be printer even
+  // if the condition is false
+  console.log("Value of i:", i);
+  i++;
+} while (i < 20);
